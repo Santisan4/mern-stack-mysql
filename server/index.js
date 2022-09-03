ca09d5df7eb1e9bc2a2c74619577ad665138ccaf
santiago <sant97@hotmail.com.ar>
2022-09-03 10:38:54 -0300

    modified: server/index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const moment = require('moment')
 const cors = require("cors")
 const path = require("path")
 
-const PORT = require("./config")
+// const PORT = require("./config")
 
 const indexRoutes = require("./routes/indexRoutes")
 const taskRoutes = require("./routes/tasksRoutes")
@@ -12,11 +12,12 @@ const taskRoutes = require("./routes/tasksRoutes")
 app.use(cors())
 app.use(express.json())
 
+app.use("/", taskRoutes)
 app.use(indexRoutes)
-app.use(taskRoutes)
 
 app.use(express.static(path.join(__dirname,"../client/dist")))
 
-app.listen(process.env.PORT || 4000)
+app.listen((process.env.PORT || 4000),() => {
+    console.log("Server is listening on port", process.env.PORT || 4000 )
+})
 
-console.log("Server is listening on port", process.env.PORT || 4000 )
